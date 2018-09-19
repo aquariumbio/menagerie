8e24b7422fde2bcbc67cc40225452e1ae04c0122
Devin Strickland <dvn.strcklnd@gmail.com>
2018-09-19 10:25:29 -0700

    moved protstab logic into plan step class

---
+++ xplan/protstab_planner.py
@@ -0,0 +1,58 @@
+import sys
+import os
+from datetime import datetime, timedelta
+import warnings
+warnings.filterwarnings('ignore')
+
+ext_plan_path = '/Users/devin/Documents/work/ext-plan-pydent'
+sys.path.append(ext_plan_path)
+
+from aq_classes import Cursor, Leg
+from xplan.xplans import XPlan
+
+from prot_stab_legs import SortLeg
+
+from plan_tests import test_plan
+from user_input import get_input
+
+inputs = get_input()
+
+start_date = inputs['start_date']
+plan = XPlan(inputs['aq_plan_name'], inputs['aq_instance'])
+
+cursor = Cursor(x=64, y=1168)
+
+for step_id in plan.step_ids(plan.get_steps_by_type('protstab_round')):
+    plan_step = plan.step(step_id)
+    plan_step.create_step(cursor, start_date)
+
+    cursor.update_max_x()
+    cursor.decr_y(SortLeg.length() + 3)
+    cursor.set_y_home()
+
+    if start_date.weekday() == 4:
+        incr = 3
+    else:
+        incr = 2
+
+    start_date += timedelta(days=incr)
+
+    print(plan_step.name + ' complete')
+    print()
+
+plan.launch_aq_plan()
+plan.add_data_associations()
+
+url = plan.aq_plan.session.url + "/plans?plan_id={}".format(plan.aq_plan.id)
+print("Created Plan: {}".format(url))
+print("{} total operations.".format(len(plan.aq_plan.operations)))
+print("{} total wires.".format(len(plan.aq_plan.wires)))
+
+# out_path = os.path.join(plan.plan_path, 'dump.json')
+# ref_path = os.path.join(plan.plan_path, 'dump-ref.json')
+# test_plan(plan, out_path, ref_path)
+# print("Test passed!")
+
+delete = input("Do you want to delete this plan? (y/n) ")
+if delete == 'y' or delete == 'Y':
+    plan.aq_plan.delete()
--- xplan/xplans.py
+++ xplan/xplans.py
@@ -2,17 +2,19 @@ import json
 import re
 import os
 
-from plans import ExternalPlan, PlanStep, Transformation
-
 import pydent
 from pydent import models
 from pydent.models import Sample
+
+from plans import ExternalPlan, PlanStep, Transformation
+from prot_stab_legs import OvernightLeg, NaiveLeg, InductionLeg, MixCulturesLeg
+from prot_stab_legs import SortLeg, FlowLeg, ProtStabLeg
 
 class XPlan(ExternalPlan):
     def __init__(self, aq_plan_name, aq_instance):
         super().__init__(aq_plan_name, aq_instance)
 
-        self.steps = [XPlanStep(self, s) for s in self.plan['steps']]
+        self.steps = [YeastDisplayStep(self, s) for s in self.plan['steps']]
 
         self.input_samples = {}
         for plan_id, aq_id in self.plan_params['input_samples'].items():
@@ -33,8 +35,6 @@ class XPlan(ExternalPlan):
 
         # Assumes that there is only one source and only one dna_seq_step
         self.ngs_samples = self.dna_seq_steps()[0].measured_samples
-
-        # self.set_default_protease()
 
     def find_input_sample(self, aq_id):
         if isinstance(aq_id, int):
@@ -59,33 +59,6 @@ class XPlan(ExternalPlan):
     def step_ids(self, steps=None):
         steps = steps or self.steps
         return sorted([s.step_id for s in steps])
-
-    ##### These are probably unique to protein stability #####
-
-    # def set_default_protease(self):
-    #     def_prot_pat = re.compile(r'protease_1', re.IGNORECASE)
-
-    #     possible_defaults = self.raw_protease_inputs()
-    #     test = [s for s in possible_defaults if re.search(def_prot_pat, s)]
-
-    #     if test:
-    #         unprovisioned = test[0]
-
-    #     else:
-    #         unprovisioned = possible_defaults[0]
-
-    #     self.default_protease = self.get_provisioned(unprovisioned)
-
-    # def raw_protease_inputs(self):
-    #     protease_inputs = []
-    #     prot_pat = re.compile(r'protease', re.IGNORECASE)
-
-    #     for t in self.provision_steps()[0].transformations:
-    #         for s in t.source:
-    #             if re.search(prot_pat, s['sample']):
-    #                 protease_inputs.append(s['sample'])
-
-    #     return list(set(protease_inputs))
 
     def protease_sample(self, s):
         return isinstance(s, Sample) and s.sample_type.name == "Protease"
@@ -122,7 +95,6 @@ class XPlanStep(PlanStep):
 
         self.output_operations = {}
 
-        # self.flow_samples = [m.source for m in self.measurements if m.file.endswith('.fcs')]
 
     def yeast_inputs(self):
         yeast_handles = ["DNA Library", "Yeast Strain"]
@@ -140,6 +112,172 @@ class XPlanStep(PlanStep):
     def get_sorted_transformations(self):
         txns = self.operator.get('transformations')
         return txns
+
+
+class YeastDisplayStep(XPlanStep):
+    def __init__(self, plan, plan_step):
+        super().__init__(plan, plan_step)
+
+    def create_step(self, cursor, start_date):
+        if int(self.step_id) > 1:
+            prev_plan_step = self.plan.step(self.step_id - 1)
+            prev_step_outputs = prev_plan_step.output_operations
+        else:
+            prev_step_outputs = {}
+
+        new_inputs = {}
+
+        for input_yeast in self.yeast_inputs():
+            st_name = self.plan.input_samples[input_yeast].sample_type.name
+            is_library = st_name == 'DNA Library'
+
+            if not prev_step_outputs.get(input_yeast):
+                cursor.incr_y(2)
+                container_opt = 'library start' if is_library else 'control'
+
+                overnight_samples = []
+                library_composition = self.plan.input_samples.get("library_composition")
+                mix_cultures = is_library and library_composition
+
+                if mix_cultures:
+                    cursor.incr_y()
+                    for comp in library_composition["components"]:
+                        overnight_samples.append(comp)
+
+                else:
+                    overnight_samples.append(self.plan.input_samples[input_yeast])
+
+                overnight_ops = {}
+
+                for os in overnight_samples:
+                    overnight_leg = OvernightLeg(self, cursor)
+                    overnight_leg.set_yeast_from_sample(os)
+                    overnight_leg.add(container_opt)
+
+                    subs = (input_yeast, str(start_date.date()))
+                    overnight_leg.set_start_date(start_date.date())
+
+                    overnight_ops[os.name] = overnight_leg.get_innoculate_op()
+                    print("Planning innoculation of %s on %s" % subs)
+
+                    cursor.incr_x()
+                    cursor.incr_y()
+
+                if mix_cultures:
+                    cursor.decr_y()
+                    cursor.return_x()
+                    mix_cultures_leg = MixCulturesLeg(self, cursor)
+                    mix_cultures_leg.set_yeast(input_yeast)
+                    mix_cultures_leg.set_components(library_composition)
+                    mix_cultures_leg.add(container_opt)
+
+                    mix_cultures_op = mix_cultures_leg.select_op('Mix Cultures')
+                    culture_inputs = mix_cultures_op.input_array("Component Yeast Culture")
+
+                    for culture in culture_inputs:
+                        op = overnight_ops[culture.sample.name]
+                        mix_cultures_leg.wire_ops(op.output("Yeast Culture"), culture)
+
+                    upstr_op = mix_cultures_op
+
+                else:
+                    upstr_op = overnight_leg.select_op('Innoculate Yeast Library')
+
+                cursor.return_y()
+
+                if input_yeast in self.plan.ngs_samples:
+                    cursor.decr_y(SortLeg.length() - NaiveLeg.length())
+                    naive_leg = NaiveLeg(self, cursor)
+                    naive_leg.set_yeast(input_yeast)
+                    naive_leg.add('library')
+                    cursor.return_y()
+
+                    dnstr_op = naive_leg.select_op('Store Yeast Library Sample')
+                    naive_leg.wire_ops(upstr_op, dnstr_op)
+
+                new_inputs[input_yeast] = upstr_op
+
+            upstr_op = prev_step_outputs.get(input_yeast) or new_inputs.get(input_yeast)
+
+            cursor.set_x(upstr_op.x)
+
+            if int(self.step_id) > 1 and is_library:
+                cursor.decr_x(2)
+            else:
+                cursor.incr_x()
+
+            cursor.return_y()
+
+            cursor.incr_y(2)
+            container_opt = 'library' if is_library else 'control'
+            induction_leg = InductionLeg(self, cursor)
+            induction_leg.set_yeast(input_yeast)
+            induction_leg.add(container_opt)
+            cursor.return_y()
+
+            dnstr_op = induction_leg.select_op('Dilute Yeast Library')
+            induction_leg.wire_ops(upstr_op, dnstr_op)
+
+            txns = [t for t in self.transformations if input_yeast in t.source_samples()]
+
+            partitioned = {}
+
+            for t in txns:
+                sample = t.protease().get('sample', '')
+
+                if not partitioned.get(sample):
+                    partitioned[sample] = []
+
+                partitioned[sample].append(t)
+
+            proteases = list(partitioned.keys())
+            proteases.sort()
+
+            for p in proteases:
+                txns = partitioned[p]
+                txns.sort(key=lambda t: t.protease().get('concentration', 0))
+
+                for txn in txns:
+                    src = txn.source
+                    for dst in txn.destination:
+                        ngs_sample = dst['sample'] in self.plan.ngs_samples
+                        if ngs_sample:
+                            this_leg = SortLeg(self, cursor)
+                        else:
+                            this_leg = FlowLeg(self, cursor)
+
+                        this_leg.set_yeast(input_yeast)
+                        this_leg.set_protease(src)
+
+                        # This is not a good way to set these variables
+                        if ngs_sample:
+                            this_leg.sample_io['Control?'] = 'no'
+                        else:
+                            yeast_name = this_leg.sample_io['Labeled Yeast Library'].name
+                            if yeast_name == 'EBY100 + pETcon3':
+                                this_leg.sample_io['Control?'] = 'autofluorescence'
+                            elif yeast_name == 'AMA1-best':
+                                if this_leg.sample_io['Protease Concentration'] == 0:
+                                    this_leg.sample_io['Control?'] = 'high-fitc'
+                                else:
+                                    this_leg.sample_io['Control?'] = 'protease'
+
+                        this_leg.add(container_opt)
+
+                        upstr_op = induction_leg.select_op('Dilute Yeast Library')
+                        dnstr_op = this_leg.select_op('Challenge and Label')
+                        this_leg.wire_ops(upstr_op, dnstr_op)
+
+                        output_op = this_leg.get_innoculate_op()
+
+                        if output_op:
+                            self.add_output_operation(dst['sample'], output_op)
+                            self.plan.add_input_sample(dst['sample'], output_op.output('Yeast Culture').sample)
+
+                        cursor.incr_x()
+                        cursor.return_y()
+
+                cursor.incr_x(0.25)
 
 
 class XPlanTransformation(Transformation):
