a5897e74b7891eccdc941f29e9afb52625e9c046
Devin Strickland <strcklnd@uw.edu>
2019-11-04 17:05:35 +0000

    updated qPCR primer name

--- xplan/xplans.py
+++ xplan/xplans.py
@@ -209,7 +209,7 @@ class DNASeqStep(XPlanStep):
         super().__init__(plan, plan_step)
 
     def create_step(self, cursor):
-        qpcr_2_forward_primer = self.plan.session.Sample.find_by_name("forward primer")
+        qpcr_2_forward_primer = self.plan.session.Sample.find_by_name("Petcon NGS prep forward primer")
         qpcr_2_reverse_primers = self.plan.input_samples.pop("qpcr_2_reverse_primers")
 
         items = list(self.plan.input_samples.values())
