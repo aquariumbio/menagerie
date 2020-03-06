3a046d5d5841dc5093c2a97aae36eb3c397238ea
Devin Strickland <strcklnd@uw.edu>
2020-03-06 05:34:06 +0000

    script for adding test samples

---
+++ util/load_test_samples.py
@@ -0,0 +1,23 @@
+import json
+import pydent
+from pydent import models
+from pydent.models import Sample, Item
+
+from pydent_helper import create_session, prettyprint
+
+def main():
+    session = create_session("laptop")
+
+    with open("yeast_display_test_samples.json", "r") as f:
+        data = json.load(f)
+
+    for s in data.get("samples", []):
+        st_name = s.pop("sample_type")
+        sample_type = session.SampleType.find_by_name(st_name)
+        s["sample_type_id"] = sample_type.id
+        
+        new_sample = session.Sample.new(**s)
+        new_sample.save()
+
+if __name__ == "__main__":
+    main()
