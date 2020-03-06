205cc3a2f603bdc3f765349e068eb459cc3e11af
Devin Strickland <strcklnd@uw.edu>
2020-03-06 18:28:16 +0000

    made load_test_samples.py report on samples loaded

--- util/load_test_samples.py
+++ util/load_test_samples.py
@@ -15,9 +15,14 @@ def main():
         st_name = s.pop("sample_type")
         sample_type = session.SampleType.find_by_name(st_name)
         s["sample_type_id"] = sample_type.id
-        
+
         new_sample = session.Sample.new(**s)
         new_sample.save()
+
+    samples = session.Sample.all()
+    print("Loaded {} Samples:".format(len(samples)))
+    for s in samples:
+        print(s.name)
 
 if __name__ == "__main__":
     main()
