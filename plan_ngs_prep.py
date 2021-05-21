46ff0d9b71c6ee59dcf158bb1789fd0817f96a74
Devin Strickland <strcklnd@uw.edu>
2021-05-21 13:57:03 +0000

    reorganization of files

--- plan_ngs_prep.py
+++ plan_ngs_prep.py
@@ -10,7 +10,7 @@ def main():
         # Override get_input() for convenience when testing code
         print_blue("RUNNING IN TEST MODE")
         inputs = {
-            'plan_path': "yeast_display_plans/test_ngs_prep_params",
+            'plan_path': "yeast_display_plans/template_ngs_prep_params",
             'aq_instance': "laptop"
         }
     else:
